7e68fe8201d2d92e09918c99f00b81a6f55193ae
jamesduncan2232 <james.duncan2557@gmail.com>
2026-07-04 17:59:54 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LongTerm from './LongTerm.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LongTerm />
+  </React.StrictMode>,
+)
